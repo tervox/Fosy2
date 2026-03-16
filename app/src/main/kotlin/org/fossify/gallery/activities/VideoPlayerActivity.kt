0851692d3beeed919cf3fce9ae65180aeead4f63
tervox <marcosnozz@hotmail.com>
2026-03-16 20:00:14 -0300

    fix: careful implementation of software decoding without breaking references

--- app/src/main/kotlin/org/fossify/gallery/activities/VideoPlayerActivity.kt
+++ app/src/main/kotlin/org/fossify/gallery/activities/VideoPlayerActivity.kt
@@ -18,6 +18,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import androidx.media3.exoplayer.DefaultRenderersFactory
+import androidx.media3.exoplayer.DefaultRenderersFactory
 import android.util.DisplayMetrics
 import android.view.GestureDetector
 import android.view.HapticFeedbackConstants
