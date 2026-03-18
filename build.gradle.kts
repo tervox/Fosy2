79f3b7c05f14f4d415ca5595533a7f5187d99eb4
tervox <marcosnozz@hotmail.com>
2026-03-18 01:45:27 -0300

    fix: isolated dependencies block for media3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
-implementation("androidx.media3:media3-exoplayer:1.2.1")
     alias(libs.plugins.android).apply(false)
     alias(libs.plugins.ksp).apply(false)
     alias(libs.plugins.detekt).apply(false)
+}
+
+dependencies {
+    implementation("androidx.media3:media3-exoplayer:1.2.1")
 }
