b5742e226e75510f121ed04e4b870ca2c43920bc
tervox <marcosnozz@hotmail.com>
2026-03-18 01:50:43 -0300

    fix: move seek function below player declaration

--- app/src/main/kotlin/org/fossify/gallery/activities/VideoPlayerActivity.kt
+++ app/src/main/kotlin/org/fossify/gallery/activities/VideoPlayerActivity.kt
@@ -14,8 +14,8 @@ import org.fossify.commons.helpers.ensureBackgroundThread
 import org.fossify.gallery.R
 
 class VideoPlayerActivity : SimpleActivity(), TextureView.SurfaceTextureListener {
-    private fun applyGooglePhotosSeek() { player?.setSeekParameters(SeekParameters.CLOSEST_SYNC) }
     private var player: ExoPlayer? = null
+    private fun applyGooglePhotosSeek() { player?.setSeekParameters(androidx.media3.exoplayer.SeekParameters.CLOSEST_SYNC) }
     private lateinit var textureView: TextureView
     private var path: String = ""
     private var uri: Uri? = null
